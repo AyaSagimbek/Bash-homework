fb82b26c5eb21e1495f55dc9f1ff55ef0f67df26
AyaSagimbek <sagimbekayaulym@gmail.com>
2025-03-03 20:07:25 +0000

    Files from HW12

---
+++ Homework-12/age.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+read -p "Enter your year of birth: " year
+
+echo "In 2025 you are " $((2025-year)) 
+echo "in 2020 you was " $((2020-year)) 
+echo "In 2030 you will be "$((2030-year))
